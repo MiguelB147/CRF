bde9a15b0510d6fbea76bc96cdf8f166f6c64afd
Miguel Beynaerts <miguel.beynaerts@gmail.com>
2025-01-18 23:35:30 +0100

    Some changes to CRF code

--- GaussianCRF.R
+++ GaussianCRF.R
@@ -8,10 +8,21 @@ sim <- rCopula(100, normalCopula(rho, dim = 2))
 u <- sim[,1]
 v <- sim[,2]
 
-C00 <- pmvnorm(upper = c(qnorm(u)), qnorm(v), corr = matrix(rho,1,1,rho), byrow=TRUE) # TODO Ga dit na in de code van de masterproef
-C10 <- pnorm((qnorm(v) - rho*qnorm(u))/sqrt(1-rho^2))
-C01 <- pnorm((qnorm(u) - rho*qnorm(v))/sqrt(1-rho^2))
-C11 <- (1/sqrt(1-rho^2))*(1/dnorm(qnorm(u)))*dnorm((qnorm(u) - rho*qnorm(v))/sqrt(1-rho^2))
-CRF <- C00*C11/(C01*C10)
+normalCRF(u,v,rho)
+
+normalCRF <- function (u, v, rho) {
+  
+  sim <- cbind(u,v)
+  
+  C00 <- apply(sim, 1, function (x) pmvnorm(lower = c(-Inf, -Inf), upper = x, corr = matrix(c(1,rho,rho,1), byrow=TRUE, ncol=2))) # TODO Ga dit na in de code van de masterproef
+  C10 <- pnorm((qnorm(v) - rho*qnorm(u))/sqrt(1-rho^2))
+  C01 <- pnorm((qnorm(u) - rho*qnorm(v))/sqrt(1-rho^2))
+  C11 <- (1/sqrt(1-rho^2))*(1/dnorm(qnorm(u)))*dnorm((qnorm(u) - rho*qnorm(v))/sqrt(1-rho^2))
+  CRF <- C00*C11/(C01*C10)
+  
+  return(CRF)
+}
+
+
 
 set.seed(123)
