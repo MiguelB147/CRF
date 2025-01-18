ae51c85ccda5b5cbf4d7a7da671bcf80a7bd99a6
Miguel Beynaerts <miguel.beynaerts@gmail.com>
2025-01-18 23:21:14 +0100

    New code to simulate a Gaussian CRF

--- GaussianCRF.R
+++ GaussianCRF.R
@@ -8,7 +8,7 @@ sim <- rCopula(100, normalCopula(rho, dim = 2))
 u <- sim[,1]
 v <- sim[,2]
 
-C00 <- pmvnorm(upper = c(qnorm(u)), qnorm(v), corr = matrix(rho,1,1,rho), byrow=TRUE)
+C00 <- pmvnorm(upper = c(qnorm(u)), qnorm(v), corr = matrix(rho,1,1,rho), byrow=TRUE) # TODO Ga dit na in de code van de masterproef
 C10 <- pnorm((qnorm(v) - rho*qnorm(u))/sqrt(1-rho^2))
 C01 <- pnorm((qnorm(u) - rho*qnorm(v))/sqrt(1-rho^2))
 C11 <- (1/sqrt(1-rho^2))*(1/dnorm(qnorm(u)))*dnorm((qnorm(u) - rho*qnorm(v))/sqrt(1-rho^2))
